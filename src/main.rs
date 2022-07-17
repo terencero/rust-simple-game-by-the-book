70357ed0d0260c875d26735e1f58a9aa49c6e20d
Terence Ro <rowyourboat83@gmail.com>
2022-07-16 21:47:08 -0500

    don't understand reference passing yet... add guess count hint

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number! 1 to 100");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut attempts: u32 = 0;
 
     loop {
         println!("Please input your guess.");
+        println!("secret {secret_number}");
     
         let mut guess = String::new();
-    
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
@@ -24,12 +26,30 @@ fn main() {
         println!("You guessed {guess}");
     
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => {
+                attempts += 1;
+                emit_too_small(&mut attempts);
+            }
             Ordering::Equal => {
-                println!("You win!");
+                emit_you_win();
                 break;
             }
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Greater => {
+                attempts += 1;
+                emit_too_big(&mut attempts);
+            }
         }
     }
+}
+
+fn emit_too_small(attempts: &mut u32) {
+    println!("Too small! attempts at guess {attempts}");
+}
+
+fn emit_too_big(attempts: &mut u32) {
+    println!("Too big! attempts at guess {attempts}");
+}
+
+fn emit_you_win() {
+    println!("You win!");
 }
