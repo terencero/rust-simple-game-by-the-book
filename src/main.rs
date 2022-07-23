e08c60de533f6d53d04f85595512dacfd4afe021
Terence Ro <rowyourboat83@gmail.com>
2022-07-23 11:56:23 -0500

    add fibonacci finder

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("Play the Guessing Game [1]");
     println!("Convert the current temperature [2]");
     println!("Sing me a Christmas song [3]");
+    println!("Print the fibonacci sequence [4]");
     let mut answer = String::new();
 
     io::stdin()
@@ -21,6 +22,8 @@ fn main() {
         convert_temperature();
     } else if answer == "3" {
         print_the_lyrics_to_the_twelve_days_of_christmas();
+    } else if answer == "4" {
+        calculate_fibonacci()
     } else {
         println!("Aww you didn't choose an option... Till next time!");
     }
@@ -189,34 +192,50 @@ fn calculate_fibonacci() {
         .read_line(&mut upper_range)
         .expect("Failed to read line");
 
-    let upper_range = upper_range.trim().parse().expect("Not a number");
-    
-    let first_fibonacci_number = for num in lower_range..upper_range {
-        let fibonacci_potential = 5 * num * num + 4;
-        let is_perfect_square = test_is_perfect_square(fibonacci_potential);
+    let upper_range: u32 = upper_range.trim().parse().expect("Not a number");
+    let range = (lower_range..upper_range).len();
+    let mut current_fibonacci_test_num = lower_range;
+    let mut counter = 0;
+
+    loop {
+        if counter > range {
+            println!("Out of range, finished.");
+            break;
+        }
+        let fibonacci_potential = 5 * current_fibonacci_test_num * current_fibonacci_test_num + 4;
+        let is_perfect_square = if test_is_perfect_square(fibonacci_potential) {
+            true
+        } else {
+            let fibonacci_potential = 5 * current_fibonacci_test_num * current_fibonacci_test_num - 4;
+            test_is_perfect_square(fibonacci_potential)
+        };
         
         if is_perfect_square {
-            lower_range;
+            println!("{current_fibonacci_test_num}");
         }
+
+        current_fibonacci_test_num += 1;
+        counter += 1;
     };
-    
 }
 
 fn test_is_perfect_square(num: u32) -> bool {
+    // iterative way to find square root instead of using sqrt method
     let start: f64 = num.into();
-    let mut test_num: f64 = num.into();
-    let mut is_square = false;
-
-    while test_num > 0.0 {
-        test_num = (test_num - 1.0) + start / (test_num - 1.0) / 2.0;
-        if test_num == start {
-            is_square = true;
-            break;
+    let mut test_num: f64 = start - 1.0;
+    let mut previous_test_num: f64 = 0.0;
+    
+    let calculated_square_root = loop {
+        test_num = (test_num + start / test_num )/ 2.0;
+        if test_num == previous_test_num {
+            break test_num;
         }
+        previous_test_num = test_num;
+    };
+
+    if start % calculated_square_root > 0.0 {
+        return false;
     }
-    is_square
-}
 
-fn test_fibonacci(num: ) {
-
+    true
 }
